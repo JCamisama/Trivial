101e91d7cf497301d8cf27a660d83b55d61c55e7
GySyX <GySyX@LAPTOP-K6JCCTH2>
2019-04-20 13:22:52 +0200

    Jokalari klasearen edizioa proba

--- Trivial/src/packtribial/Jokalaria.java
+++ Trivial/src/packtribial/Jokalaria.java
@@ -1,9 +1,20 @@
 package proiektua;
 
+
 public class Jokalaria {
-	//atributuak-atributos
+	
+	//Atributuak
+	private String			kolorea;
+	private ListaGaztatxoak	puntuak;
+	private int				posizioa;
+	private int 			pasilloa;
+	
+	
+	//Eraikitzailea
+	
+	public Jokalaria(String pKolorea, ListaGaztatxoak)
+	
 
-	//eraikitzailea-constructora
+}
 
-	//gainontzeko metodoak-metodos adicionales
 }
